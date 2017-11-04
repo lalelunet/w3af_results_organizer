8b6be035e054fbe734d25110b0acb58cbe5045aa
lalelunet <github@eckert.ninja>
2017-11-04 22:04:00 +0100

    delete angular. I can not learn this never ever....

--- static/app/main.ts
+++
@@ -1,5 +0,0 @@
-import { bootstrap }    from '@angular/platform-browser-dynamic';
-//import { APP_ROUTER_PROVIDERS } from './app.routes';
-import { AppComponent } from './main/app.component';
-
-bootstrap(AppComponent);
